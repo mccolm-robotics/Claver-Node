8fde6d11145dfc242437ff9fd3a81c612ba86d39
praxis <simulacra.mechatronics@gmail.com>
2020-10-02 15:46:17 -0700

    Bumped to v0.1.5

--- src/ClaverNode.py
+++ src/ClaverNode.py
@@ -1,58 +1,11 @@
 #!/usr/bin/python
-
-import json
 import sys
 import gi
-import websockets
-
 gi.require_version('Gtk', '3.0')
-from gi.repository import Gtk, GLib, Gio
+from gi.repository import Gtk
 import asyncio, threading
 
-class ClaverWebsocket:
-    def __init__(self, claverNode):
-        self.claverNode = claverNode
-        self.uri = "ws://localhost:6789"
-        self.loop = asyncio.new_event_loop()
-        asyncio.set_event_loop(self.loop)
-
-    def get_loop(self):
-        return self.loop
-
-    async def __authenticate_connection(self, websocket: websockets) -> bool:
-        credentials = json.dumps({"agent": 'browser', "bid": 'cb6070bf-e9aa-11ea-97ab-7085c2d6de42', "token": '958058', "mode": 'WhiteBoard'})
-        await websocket.send(credentials)
-        response = await websocket.recv()
-        data = json.loads(response)
-        GLib.idle_add(self.claverNode.update_gui, data)
-        return True
-
-    async def send_data(self, data):
-        message = json.dumps(data)
-        await self.websocket.send(message)
-
-    async def __run(self):
-        authenticated = False
-        async with websockets.connect(self.uri) as websocket:
-            self.websocket = websocket
-            while True:
-                try:
-                    if not authenticated:
-                        authenticated = await self.__authenticate_connection(websocket)
-                    else:
-                        message = await websocket.recv()
-                        data = json.loads(message)
-                        GLib.idle_add(self.claverNode.update_gui, data)
-                except websockets.ConnectionClosed:
-                    break
-
-    def run_asyncio(self):
-        try:
-            self.loop.run_until_complete(self.__run())
-        except KeyboardInterrupt:
-            pass
-        except ConnectionRefusedError:
-            print("Connection refused. Server offline")
+from src.ClaverWebsocket import ClaverWebsocket
 
 
 class ClaverNode(Gtk.Application):
